bc21ed2e7adb3fd293d77639a7f02d6562806cd3
DillJoker5 <79213574+DillJoker5@users.noreply.github.com>
2022-03-30 19:31:25 -0500

    Updated ReadMe and added default values for Review Table

--- Creation Scripts/Tables/review.sql
+++ Creation Scripts/Tables/review.sql
@@ -6,7 +6,7 @@ CREATE TABLE Review (
 );
 
 INSERT INTO Review
-VALUES (),
-(),
-(),
-(),
+VALUES ('Test', 'This is a test description. I do not care about this house whatsoever!', 12345678),
+('Dylan Chirigotis', 'I love this house. The decor and everything is perfect. 10/10 would recommend!', 12345678),
+('Anna Kuether', 'This house is in a terrible area, is trashy, and is falling apart. 0/10', 12345612),
+('Erika Fahney', 'This house is probably one of the worst houses I have ever looked at. Do not visit here unless you want to be disappointed.', 12345612)
--- README.md
+++ README.md
@@ -3,12 +3,14 @@ SQL Scripts for CSC 485 Capstone Project
 
 # Tables
 ## House
-HouseId
-Price
-HouseLocation
-Distance
-ReviewId
-
+HouseId : Bigint : PK
+Price : Float : Cannot be NULL
+HouseLocation : Varchar of 200 : Cannot be NULL
+Distance : Float
+ReviewId : Bigint : FK to ReviewId of Review
 ## Review
-
+ReviewId : Bigint : PK
+NameOfUser : Varchar of 200 : Cannot be NULL
+Description : Varchar of 200
+HouseId : Bigint : FK to HouseId of House
 ## User
