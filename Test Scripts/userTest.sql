68187a0e1d3114b6adbecf50c058ec5d34334e92
Dylan Chirigotis <dylan.chirigotis@mail.wlc.edu>
2022-04-11 21:31:06 -0500

    Updated Users Table test file

--- Test Scripts/userTest.sql
+++ Test Scripts/userTest.sql
@@ -1,42 +1,42 @@
 SELECT *
-FROM User
+FROM Users
 
 SELECT Username, Name, Password
-FROM User
+FROM Users
 
 SELECT *
-FROM User u
+FROM Users u
 WHERE u.Username = 'DTC101'
 
 SELECT *
-FROM User u
+FROM Users u
 WHERE u.Name = 'Admin'
 
 SELECT *
-FROM User u
+FROM Users u
 WHERE u.Password = 'lovingTheBestest456'
 
 SELECT *
-FROM User u
+FROM Users u
 ORDER BY u.Name
 
 SELECT *
-FROM User u
+FROM Users u
 
 SELECT u.Username, u.Name
-FROM User u
+FROM Users u
 WHERE u.Username NOT IN (
     SELECT u1.Username
-    FROM User u1
+    FROM Users u1
     WHERE u1.Username = 'Administrator'
 )
 
 SELECT *
-FROM User u
+FROM Users u
 INNER JOIN House h on h.HouseId = u.HouseId
 
 SELECT u.Username, u.Name, h.HouseLocation
-FROM User u
+FROM Users u
 INNER JOIN House h on h.HouseId = u.HouseId
 GROUP BY (u.Username), u.Name, h.HouseLocation
 ORDER BY (u.Username), u.Name, h.HouseLocation
