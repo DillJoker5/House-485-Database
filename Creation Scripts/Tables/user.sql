bdf15637fed53912dacf572dceacd5c46ed32d5f
Dylan Chirigotis <dylan.chirigotis@mail.wlc.edu>
2022-04-24 12:48:16 -0500

    Redid house and users table

--- Creation Scripts/Tables/user.sql
+++ Creation Scripts/Tables/user.sql
@@ -2,11 +2,10 @@ CREATE TABLE Users (
     UserId BIGINT IDENTITY(1,1) PRIMARY KEY,
     Username VARCHAR(100) NOT NULL,
     Name VARCHAR(100) NOT NULL,
-    Password VARCHAR(100) NOT NULL,
-    HouseId BIGINT FOREIGN KEY (HouseId) REFERENCES House(HouseId)
+    Password VARCHAR(100) NOT NULL
 );
 
 INSERT INTO Users
-VALUES ('Administrator', 'Admin', 'asdcvasdqwe123', 1),
-('DTC101', 'Dylan Chirigotis', 'lovingTheBestest456', 1),
-('AK4422', 'Anna Kuether', 'asdfblmasfj08123', 2)
+VALUES ('Administrator', 'Admin', 'asdcvasdqwe123'),
+('DTC101', 'Dylan Chirigotis', 'lovingTheBestest456'),
+('AK4422', 'Anna Kuether', 'asdfblmasfj08123')
--- README.md
+++ README.md
@@ -7,12 +7,12 @@ HouseId -- Bigint -- Primary Key <br>
 Price -- Float -- Cannot be NULL <br>
 HouseLocation -- Varchar of 200 -- Cannot be NULL <br>
 Distance -- Float <br>
+UserId -- Bigint -- Foreign Key to UserId of Users <br>
 ## Users
 UserId -- Bigint -- Primary Key <br>
 Username -- Varchar of 100 -- Not NULL <br>
 Name -- Varchar of 100 -- Not NULL <br>
 Password -- Varchar of 100 -- Not NULL <br>
-HouseId -- Bigint -- Foreign Key to HouseId of House <br>
 ## Session
 SessionId -- Bigint -- Primary Key <br>
 UserId -- Bigint -- Foreign Key to UserId of Users <br>
